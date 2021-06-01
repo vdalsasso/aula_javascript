5ccfcdfccc56f09b0abb0101e4b9f0322a447c47
vdalsasso <vin.dalsasso@gmail.com>
2021-05-31 22:29:01 -0300

    funcoes

---
+++ js/main.js
@@ -0,0 +1,53 @@
+function soma(n1, n2) {
+    return n1 + n2;
+    
+}
+
+//var d = new Date();
+//alert(d.getMonth());
+
+
+/*
+var count = 0;
+
+for(count=0; count <=5; count++) {
+    alert(count);
+}*/
+
+/*var count = 5;
+while (count < 5) {
+    console.log(count);
+    count++;
+}
+*/
+
+/*
+var idade = prompt("Qual sua idade?");
+
+if(idade > 18) {
+    alert("maior de idade");
+} else {
+    alert("menor de idade");
+}*/
+
+
+
+//var fruta = {nome: "maçã", cor: "vermelha"};
+//console.log(fruta.nome);
+
+//var lista = ["maçã", "pêra", "laranja"];
+//lista.push("uva");
+//ista.pop();
+//console.log(lista[1]);
+
+
+//var nome = "Vinicius Dalsasso"
+//var idade = 26;
+//var idade2 = 10;
+//var frase = "Japao eh o melhor time"
+
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(idade + idade2);
+//console.log(frase.replace("Japao", "Brasil"));
